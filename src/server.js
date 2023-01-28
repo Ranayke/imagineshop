5b6298cce881601a8653e32c64e9ab9b68f0e67a
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-01-28 11:26:01 -0300

    feat(login): Implemented login auth

--- src/server.js
+++ src/server.js
@@ -9,35 +9,47 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  res.send('IMAGINE SHOP')
-})
+app.get("/", async (req, res) => {
+  res.send("IMAGINE SHOP");
+});
 
-app.post('/users', async (req, res) => {
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userService = new UserService();
+  const userLogged = await userService.login(email, password);
+  if (userLogged) {
+    return res.status(200).json(userLogged);
+  }
+  return res
+    .status(400)
+    .json({ message: "Não foi encontrado nenhum usuário." });
+});
+
+app.post("/users", async (req, res) => {
   const { name, email, password } = req.body;
   const user = { name, email, password };
   const userService = new UserService();
   await userService.create(user);
   return res.status(201).json(user);
-})
+});
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   const userService = new UserService();
   const users = await userService.findAll();
-  return res.status(200).json(users)
-})
+  return res.status(200).json(users);
+});
 
-app.get('/users/:id', authMiddleware, async (req, res) => {
+app.get("/users/:id", authMiddleware, async (req, res) => {
   const id = req.params.id;
   const userService = new UserService();
   const user = await userService.findById(id);
   if (user) {
     return res.status(200).json(user);
   }
-  return res.status(404).json({ message: 'Usuário não encontrado!'});
-})
+  return res.status(404).json({ message: "Usuário não encontrado!" });
+});
 
-app.put('/users/:id', async (req, res) => {
+app.put("/users/:id", async (req, res) => {
   const id = req.params.id;
   const { name, email, password } = req.body;
   const user = { name, email, password };
@@ -45,21 +57,21 @@ app.put('/users/:id', async (req, res) => {
   const findUser = await userService.findById(id);
   if (findUser) {
     await userService.update(id, user);
-    return res.status(200).json({ message: 'Usuário atualizado com sucesso!'});
+    return res.status(200).json({ message: "Usuário atualizado com sucesso!" });
   }
-  return res.status(404).json({ message: 'Usuário não encontrado!'});
-})
+  return res.status(404).json({ message: "Usuário não encontrado!" });
+});
 
-app.delete('/users/:id', async (req, res) => {
+app.delete("/users/:id", async (req, res) => {
   const id = req.params.id;
   const userService = new UserService();
   const user = await userService.findById(id);
   if (user) {
     await userService.delete(id);
-    return res.status(200).json({ message: 'Usuário excluído com sucesso!'});
+    return res.status(200).json({ message: "Usuário excluído com sucesso!" });
   }
-  return res.status(404).json({ message: 'Usuário não encontrado!' });
-})
+  return res.status(404).json({ message: "Usuário não encontrado!" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
